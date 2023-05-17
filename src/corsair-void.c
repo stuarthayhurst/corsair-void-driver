c36d3fc5ac46e3fc77929841dd440bc53cb65c5b
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-05-17 10:47:36 +0100

    Added link for issue reporting

--- src/corsair-void.c
+++ src/corsair-void.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * HID driver for Corsair Void headsets
+ * Report issues to https://github.com/stuarthayhurst/corsair-void-driver/issues
 
  * Copyright (c) 2023 Stuart Hayhurst
 */
